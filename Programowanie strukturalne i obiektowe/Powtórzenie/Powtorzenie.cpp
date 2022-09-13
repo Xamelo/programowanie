91e25fbf31d2b0cd73f053a32f3beaa44de02e72
xamel <kamilborek9@wp.pl>
2022-09-13 09:50:01 +0200

    sasfas

--- Programowanie strukturalne i obiektowe/Powtórzenie/Powtorzenie.cpp
+++ Programowanie strukturalne i obiektowe/Powtórzenie/Powtorzenie.cpp
@@ -7,13 +7,15 @@ void showMenu()
 	cout << "Menu:\n";
 	cout << "1. Pole kwadratu\n";
 	cout << "2. Pole trojkatu\n";
+
+	cout << "0. Zamknij program\n";
 }
 
 int chooseOptionsFromUser()
 {
 	int selectedOption;
 
-	cout << "Wybierz opcje\n";
+	cout << "Wybierz opcje: ";
 	cin >> selectedOption;
 	return selectedOption;
 }
@@ -26,7 +28,7 @@ void squareArea()
 	cout << "Podaj bok kwadratu: ";
 	cin >> side;
 	area = side * side;
-	cout << "Pole wynosi: " << area;
+	cout << "Pole wynosi: " << area << endl;
 }
 
 void triangleArea()
@@ -39,7 +41,7 @@ void triangleArea()
 	cout << "Podaj wysokosc trojkata: ";
 	cin >> height;
 	area = side * height / 2;
-	cout << "Pole trojkata wynosi: " << area;
+	cout << "Pole trojkata wynosi: " << area << endl;
 }
 
 void doSelctedTask(int selectedOption)
@@ -52,12 +54,15 @@ void doSelctedTask(int selectedOption)
 	case 2:
 		triangleArea();
 		break;
+	case 0:
+		return;
 	default:
 		cout << "Brak opcji w menu";
 		break;
 	}
 
-
+	system("pause");
+	
 	/* if (selectedOption == 1)
 	{
 		squareArea();
@@ -71,12 +76,16 @@ void doSelctedTask(int selectedOption)
 
 void mainProgram()
 {
-	//1. wyświetlenie menu
-	showMenu();
-	//2. wybranie opcji przez użytkownika
-	int selected = chooseOptionsFromUser();
-	//3. wykonanie wybranego zadania
-	doSelctedTask(selected);
+	int selected;
+	do
+	{
+		//1. wyświetlenie menu
+		showMenu();
+		//2. wybranie opcji przez użytkownika
+		selected = chooseOptionsFromUser();
+		//3. wykonanie wybranego zadania
+		doSelctedTask(selected);
+	} while (selected != 0);
 }
 
 void main()
