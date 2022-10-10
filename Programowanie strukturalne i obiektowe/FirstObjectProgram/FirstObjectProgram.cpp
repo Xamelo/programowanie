59e23bc2b5ebe23d08a871446f0867445dac2164
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-10 09:57:31 +0200

    koniec lekcji

--- Programowanie strukturalne i obiektowe/FirstObjectProgram/FirstObjectProgram.cpp
+++ Programowanie strukturalne i obiektowe/FirstObjectProgram/FirstObjectProgram.cpp
@@ -3,26 +3,40 @@
 class Point
 {
 public:
-    int x;
-    int y;
-
+    
     double calculateDistance()
     {
         double distance = sqrt(x * x + y * y);
         return distance;
     }
 
+    void SetX(int x)
+    {
+        //zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
+        if (x >= 0)
+            this->x = x;
+    }
+
+    void SetY(int y)
+    {
+        //zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
+        if (y >= 0)
+            this->y = y;
+    }
+
 protected:
 
 private:
-    
+    int x;
+    int y;    
 };
 
 int main()
 {
     Point point;
-    point.x = 10;
-    point.y = 15;
+    //point.x = 10;
+    point.SetX(10);
+    point.SetY(15);
     double distance = point.calculateDistance();
     std::cout << "Dystans od punktu (0;0) = " << distance << "\n";
 
