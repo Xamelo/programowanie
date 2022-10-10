3859d5f3c9ec02c9171d0d87cbc734753a19b9c2
Łukasz Felisek <lfelisek@e-zsel.kielce.eu>
2022-10-10 15:15:41 +0200

    koniec lekcji

--- Programowanie strukturalne i obiektowe/FirstObjectProgram/FirstObjectProgram.cpp
+++ Programowanie strukturalne i obiektowe/FirstObjectProgram/FirstObjectProgram.cpp
@@ -3,41 +3,59 @@
 class Point
 {
 public:
-    
-    double calculateDistance()
-    {
-        double distance = sqrt(x * x + y * y);
-        return distance;
-    }
 
-    void SetX(int x)
-    {
-        //zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
-        if (x >= 0)
-            this->x = x;
-    }
+	//metoda konstruktora obiektu
+	/*Point()
+	{
+		x = 0;
+		y = 0;
+	}*/
 
-    void SetY(int y)
-    {
-        //zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
-        if (y >= 0)
-            this->y = y;
-    }
+	Point(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+
+	double CalculateDistance()
+	{
+		double distance = sqrt(x * x + y * y);
+		return distance;
+	}
+
+	void SetX(int x)
+	{
+		//zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
+		if (x >= 0)
+			this->x = x;
+	}
+
+	void SetY(int y)
+	{
+		//zak³adamy ¿e jesteœmy tylko w pierwszej æwiartce uk³adu XY
+		if (y >= 0)
+			this->y = y;
+	}
 
 protected:
 
 private:
-    int x;
-    int y;    
+	int x;
+	int y;
 };
 
 int main()
 {
-    Point point;
-    //point.x = 10;
-    point.SetX(10);
-    point.SetY(15);
-    double distance = point.calculateDistance();
-    std::cout << "Dystans od punktu (0;0) = " << distance << "\n";
+	int x;
+	std::cout << "Podaj x\n";
+	std::cin >> x;
+
+	Point point2;
+	Point point(x, 15);
+	//point.x = 10;
+	//point.SetX(10);
+	//point.SetY(15);
+	double distance = point.CalculateDistance();
+	std::cout << "Dystans od punktu (0;0) = " << distance << "\n";
 
 }
